3c4d2a220f6a308d4902716ef0deb973170118cf
Mike Schaper <mikeschaper12@gmail.com>
2023-03-13 16:39:16 +0100

    homepage hero

---
+++ components/Head.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const PageHead = () => {
+  return (
+    <Head>
+      <title>Unveil - A curated platform for photography NFTs- </title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="Description" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default PageHead;
---
+++ components/Sections/FloatingArt.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Link from "next/link";
+
+const FloatingArt = () => {
+  return (
+    <>
+      <section className="h-[100svh] flex justify-center flex-col p-[15px] sm:p-10 relative">
+        <div className="absolute -z-10 block top-[5%] left-[20%]">
+          <div className="bg-unveilGreen w-[70px] h-[89px] sm:w-[140px] sm:h-[180px]"></div>
+          <small className="sm:block hidden l2">Batiaan Woudt</small>
+        </div>
+
+        <div className="absolute -z-10 block bottom-[20%] sm:bottom-[5%] right-[10%] left-auto sm:left-[40%]">
+          <div className="bg-unveilGreen w-[70px] h-[89px] sm:w-[140px] sm:h-[180px]"></div>
+          <small className="sm:block hidden l2">Batiaan Woudt</small>
+        </div>
+
+        <div className="absolute -z-10 block top-[10%] sm:top-0 right-[4%] sm:right-auto sm:left-1/2">
+          <div className="bg-unveilGreen w-[136px] h-[184px] sm:w-[410px] sm:h-[500px]"></div>
+          <small className="sm:block hidden l2">Batiaan Woudt</small>
+        </div>
+
+        <div className="absolute -z-10 block bottom-[0] sm:bottom-[30%] left-0 sm:left-auto sm:right-0 sm:translate-y-1/2 translate-y-0">
+          <div className="bg-unveilGreen w-[164px] h-[229px] sm:w-[320px] sm:h-[422px]"></div>
+          <small className="sm:block hidden l2">Batiaan Woudt</small>
+        </div>
+
+        <h1 className="h3 max-w-[600px]">
+          Collect photography. Empower artists.
+        </h1>
+        <div className="flex gap-[10px] mt-10">
+          <Link href="">
+            <button className="btn btn-primary">Start collecting</button>
+          </Link>
+          <Link href="">
+            <button className="btn btn-secondary">Request access</button>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default FloatingArt;
