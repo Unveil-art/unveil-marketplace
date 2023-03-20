1b0895d1e7fd93241fde87a5bd11fc7b3072d533
Mike Schaper <mikeschaper12@gmail.com>
2023-03-20 14:08:52 +0100

    navbar added

---
+++ components/svg/Account.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Account = () => {
+  return (
+    <svg
+      width="26"
+      height="26"
+      viewBox="0 0 26 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        cx="12.8712"
+        cy="8.0265"
+        r="6.40248"
+        stroke="#141414"
+        strokeWidth="3"
+      />
+      <path
+        d="M1.57812 24.485C4.28032 21.4836 8.33683 19.5752 12.872 19.5752C17.4072 19.5752 21.4637 21.4836 24.1659 24.485"
+        stroke="#141414"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+};
+
+export default Account;
