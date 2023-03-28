46891ed0d4087eb8ba470a969786c91d1cdf2f94
Baptiste Briel <baptiste@antinomy.studio>
2023-03-28 20:46:24 +0200

    added key to avoid error on build

--- components/section/SearchBlockItems.jsx
+++ components/section/SearchBlockItems.jsx
@@ -5,8 +5,8 @@ import ProductCard from "../reusable/ProductCard";
 const SearchBlockItems = () => {
   return (
     <section className="grid grid-cols-4 gap-10">
-      {[1, 1, 1, 1, 1, 1, 1, 1].map((item) => (
-        <div className="">
+      {[1, 1, 1, 1, 1, 1, 1, 1].map((item, index) => (
+        <div key={index} className="">
           <ProductCard />
         </div>
       ))}
