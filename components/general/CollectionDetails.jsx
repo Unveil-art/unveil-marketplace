b963ee146947f7df92486382169f244e21163182
Baptiste Briel <baptiste@antinomy.studio>
2023-03-29 18:18:07 +0200

    added test transition to collection details component

--- components/general/CollectionDetails.jsx
+++ components/general/CollectionDetails.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+import { useIntersection } from "@/hooks/useIntersection";
 import Animate from "@/components/reusable/Animate";
 import Currency from "@/components/svg/Currency";
 
 const CollectionDetails = ({ imageMargin, color, backgroundColor }) => {
+  const el = useRef()
+  const query = gsap.utils.selector(el)
+  const { isIntersecting } = useIntersection(el)
+
+  const animateIn = () => {
+    gsap.timeline({
+      paused: true
+    }).timeScale(1.75).fromTo(query('.gsap-transform'), {
+      xPercent: -100,
+      x: -40
+    }, {
+      xPercent: 0,
+      x: 0,
+      duration: 2.5,
+      stagger: 0.1,
+      ease: 'power3.out'
+    }, 0.25).restart()
+  }
+
+  useEffect(() => {
+    if (isIntersecting) {
+      animateIn()
+    }
+  }, [isIntersecting])
+
   return (
-    <div className="md:flex-row flex-col flex gap-[30px] relative">
+    <div ref={el} className="md:flex-row flex-col flex gap-[30px] relative">
       <div
         className={`${
           imageMargin ? "md:mb-10 md:ml-10" : ""
-        } w-full md:w-[65svw] aspect-square pr-10 md:pr-0`}
+        } relative w-full md:w-[65svw] aspect-square pr-10 md:pr-0 z-10`}
       >
         <div className="block w-full h-full bg-unveilGreen"></div>
       </div>
@@ -19,21 +46,23 @@ const CollectionDetails = ({ imageMargin, color, backgroundColor }) => {
         }
       }} className="md:ml-0 ml-10 md:mb-0 mb-[55px] sticky top-10 pb-10 left-0 h-fit">
         <h6
-          className="gsap-stagger w-fit rounded-full px-2 l2 mb-[10px] md:mb-[15px]"
+          className="gsap-transform w-fit rounded-full px-2 l2 mb-[10px] md:mb-[15px]"
           style={{ border: `solid 1px ${color}` }}
         >
           Live Drop
         </h6>
-        <p className="s2 mb-[6px] md:mb-[15px]">03:02:22</p>
-        <h3 className="h4">Collection name</h3>
-        <small className="b5">Starting price (edition of 10)</small>
-        <div className="flex items-center  mb-[15px]">
-          <p className="b3">€1200 (</p>
-          <Currency color={color} />
-          <p className="b3">1.2)</p>
+        <p className="gsap-transform s2 mb-[6px] md:mb-[15px]">03:02:22</p>
+        <h3 className="gsap-transform h4">Collection name</h3>
+        <div class="gsap-transform">
+          <small className="b5">Starting price (edition of 10)</small>
+          <div className="flex items-center  mb-[15px]">
+            <p className="b3">€1200 (</p>
+            <Currency color={color} />
+            <p className="b3">1.2)</p>
+          </div>
         </div>
         <button
-          className="gsap-stagger btn btn-primary"
+          className="gsap-transform btn btn-primary"
           style={{ backgroundColor: color, color: backgroundColor }}
         >
           View collection
