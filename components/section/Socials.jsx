6a5f9d97134e8964729ee5fc051a915971f43d99
Baptiste Briel <baptiste@antinomy.studio>
2023-03-29 16:07:03 +0200

    created useLineMaskingAnimation hook for line animation

--- components/section/Socials.jsx
+++ components/section/Socials.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import { useRef } from "react";
+import { useIntersection } from "@/hooks/useIntersection";
+import { useLineMaskingAnimation } from "@/hooks/animations/useLineMaskingAnimation";
 
 const Socials = () => {
+  const el = useRef()
+  const { isIntersecting } = useIntersection(el)
+  
+  useLineMaskingAnimation(el, isIntersecting)
+
   return (
-    <section className="p-[15px] md:p-10 md:pb-10 pb-[60px]">
+    <section ref={el} className="p-[15px] md:p-10 md:pb-10 pb-[60px]">
       <h2 className="b1">Follow us and stay in the loop</h2>
-      <div className="grid grid-cols-1 md:grid-cols-4 group mt-[130px] md:mt-[180px] border-t-[3px] md:border-t-4 border-t-unveilBlack">
-        <div className="md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
+
+      <div className="gsap-line mt-[130px] md:mt-[180px] border-t-[3px] md:border-t-4 border-t-unveilBlack"></div>
+
+      <div className="grid grid-cols-1 md:grid-cols-4 group overflow-hidden">
+        <div className="gsap-transform md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
           <p className="md:pt-6 md:mb-6 py-[17px] b3">Community</p>
           <h3 className="md:b1 h3 text-[24px]">Discord</h3>
         </div>
-        <div className="md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
+        <div className="gsap-transform md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
           <p className="md:pt-6 md:mb-6 py-[17px] b3">Community</p>
           <h3 className="md:b1 h3 text-[24px]">Discord</h3>
         </div>
-        <div className="md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
+        <div className="gsap-transform md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent  md:border-r border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
           <p className="md:pt-6 md:mb-6 py-[17px] b3">Community</p>
           <h3 className="md:b1 h3 text-[24px]">Discord</h3>
         </div>
-        <div className="md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none md:border-r group-last:border-none border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
+        <div className="gsap-transform md:mt-[10px] border-b border-b-unveilGreen md:border-b-transparent md:border-b-none md:border-r group-last:border-none border-r-unveilDrakGray md:mr-5 md:block flex items-center justify-between">
           <p className="md:pt-6 md:mb-6 py-[17px] b3">Community</p>
           <h3 className="md:b1 h3 text-[24px]">Discord</h3>
         </div>
---
+++ hooks/animations/useLineMaskingAnimation.js
@@ -0,0 +1,45 @@
+import { gsap } from "gsap";
+import { useEffect, useState } from "react";
+
+export function useLineMaskingAnimation(el, isIntersecting) {
+  const query = gsap.utils.selector(el)
+  const [hasRendered, setHasRendered] = useState(false)
+
+  const animateIn = () => {
+    gsap.timeline({
+      paused: true
+    }).timeScale(1.75).set(el.current, {
+      autoAlpha: 1
+    }, 0).fromTo(query('.gsap-line'), {
+      scaleX: 0,
+      transformOrigin: 'left center'
+    }, {
+      scale: 1,
+      duration: 1.75,
+      ease: 'power3.out'
+    }, 0).fromTo(query('.gsap-transform'), {
+      yPercent: -100,
+      y: -40
+    }, {
+      yPercent: 0,
+      y: 0,
+      duration: 2.5,
+      stagger: 0.2,
+      ease: 'power3.out'
+    }, 0.25).restart()
+  }
+
+  const animateOut = () => {
+    gsap.set(el.current, {
+      autoAlpha: 0
+    })
+  }
+
+  useEffect(() => {
+    if (isIntersecting) {
+      animateIn()
+    } else {
+      animateOut()
+    }
+  }, [isIntersecting])
+}
