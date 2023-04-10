7ace9fabef08c2b550184969bfda9217299bdeb2
Baptiste Briel <baptiste@antinomy.studio>
2023-04-10 16:03:53 +0200

    updated animateIn of index hero: animate letters only if previous history

--- components/section/FloatingArt.jsx
+++ components/section/FloatingArt.jsx
@@ -27,46 +27,50 @@ const FloatingArt = () => {
     const line = query('.gsap-line')
     const stagger = query('.gsap-stagger')
     const thumbnails = query('.gsap-thumbnail')
-    const cache = words.map((el) => el.getBoundingClientRect())
     const images = thumbnails.map((el) => el.getBoundingClientRect())
     const padding = parseInt(getComputedStyle(el.current).getPropertyValue('padding-left'))
-    const tl = gsap.timeline({
-      paused: true
-    }).timeScale(1.25).set(el.current, {
+    const tl = gsap.timeline({ paused: true })
+    tl.timeScale(1.25)
+    tl.set(el.current, {
       autoAlpha: 1
-    }, 0).set(align, {
-      y: cache[0].height * 0.5
     }, 0)
-    words.forEach((word, index) => {
-      const width = cache.reduce((sum, entry, loop) => loop <= index ? sum + entry.width : sum, 0)
-      const translate = (size.width * 0.5) - (width * 0.5) - padding
+    if (!previous) {
+      const cache = words.map((el) => el.getBoundingClientRect())
+      tl.set(align, {
+        y: cache[0].height * 0.5
+      }, 0)
+      words.forEach((word, index) => {
+        const width = cache.reduce((sum, entry, loop) => loop <= index ? sum + entry.width : sum, 0)
+        const translate = (size.width * 0.5) - (width * 0.5) - padding
+        tl.to(title, {
+          x: translate,
+          duration: index === 0 ? 0 : 0.5,
+          ease: 'expo.out'
+        }, 0.75 * index).fromTo(word, {
+          y: 20,
+          autoAlpha: 0
+        }, {
+          y: 0,
+          autoAlpha: 1,
+          duration: 0.75,
+          ease: 'expo.out'
+        }, 0.75 * index)
+      })
       tl.to(title, {
-        x: translate,
-        duration: index === 0 ? 0 : 0.5,
-        ease: 'expo.out'
-      }, 0.75 * index).fromTo(word, {
-        y: 20,
+        x: 0,
+        duration: 1,
+        ease: 'expo.inOut'
+      }).fromTo(line, {
+        x: 40,
         autoAlpha: 0
       }, {
-        y: 0,
+        x: 0,
         autoAlpha: 1,
-        duration: 0.75,
+        duration: 0.45,
         ease: 'expo.out'
-      }, 0.75 * index)
-    })
-    tl.to(title, {
-      x: 0,
-      duration: 1,
-      ease: 'expo.inOut'
-    }).fromTo(line, {
-      x: 40,
-      autoAlpha: 0
-    }, {
-      x: 0,
-      autoAlpha: 1,
-      duration: 0.45,
-      ease: 'expo.out'
-    }, '-=0.65').fromTo(stagger, {
+      }, '-=0.65')
+    }
+    tl.fromTo(stagger, {
       x: 10,
       autoAlpha: 0
     }, {
@@ -81,7 +85,6 @@ const FloatingArt = () => {
       const sign = Math.sign((top + (height * 0.5)) - (size.height * 0.5))
       const margin = sign === -1 ? top : (size.height - (top + (height * 0.5)))
       const value = (height + margin) * sign
-      // console.log({ index, value })
       tl.fromTo(el, {
         autoAlpha: 1,
         y: value
@@ -97,20 +100,19 @@ const FloatingArt = () => {
   })
 
   useEffect(() => {
+    window.scrollTo(0, 0)
     if (!previous) {
-      window.scrollTo(0, 0)
       gsap.set(el.current, {
         autoAlpha: 0
       })
-      if (!once.current && loaded && size.width && size.height) {
-        once.current = true
-        animateIn()
-      }
     }
-  }, [loaded, size, previous])
+    if (!once.current && loaded && size.width && size.height) {
+      once.current = true
+      animateIn()
+    }
+  }, [loaded, size])
 
   useLenis(({ scroll }) => {
-    // const top = rect.top - scroll
     const parallax = query('.gsap-parallax')
     parallax.forEach((el, index) => {
       const speed = el.getAttribute('data-speed')
@@ -132,7 +134,7 @@ const FloatingArt = () => {
   return (
     <>
       <section ref={el} className="h-[100svh] flex justify-center flex-col p-[15px] sm:p-10 relative overflow-hidden">
-        <div data-cursor="View Artist" data-cursor-color="#8A8E7B" className="gsap-thumbnail absolute block top-[5%] left-[20%] z-10">
+        <div data-cursor="View Artist" data-cursor-color="#8A8E7B" className="gsap-thumbnail absolute block top-[5%] left-[20%] z-10 invisible">
           <div className="gsap-parallax" data-speed="0.1">
             <div className="bg-unveilGreen w-[70px] h-[89px] sm:w-[140px] sm:h-[180px]">
               <Image
@@ -146,7 +148,7 @@ const FloatingArt = () => {
           </div>
         </div>
 
-        <div data-cursor="View Artist" data-cursor-color="#5B91AC" className="gsap-thumbnail absolute block bottom-[20%] sm:bottom-[5%] right-[10%] left-auto sm:left-[40%] sm:right-auto z-10">
+        <div data-cursor="View Artist" data-cursor-color="#5B91AC" className="gsap-thumbnail absolute block bottom-[20%] sm:bottom-[5%] right-[10%] left-auto sm:left-[40%] sm:right-auto z-10 invisible">
           <div className="gsap-parallax" data-speed="0.1">
             <div className="bg-unveilGreen w-[70px] h-[89px] sm:w-[140px] sm:h-[180px]">
               <Image
@@ -160,7 +162,7 @@ const FloatingArt = () => {
           </div>
         </div>
 
-        <div data-cursor="View Artist" data-cursor-color="#C1C1C1" className="gsap-thumbnail absolute block top-[10%] sm:top-0 right-[4%] sm:right-auto sm:left-1/2 z-10">
+        <div data-cursor="View Artist" data-cursor-color="#C1C1C1" className="gsap-thumbnail absolute block top-[10%] sm:top-0 right-[4%] sm:right-auto sm:left-1/2 z-10 invisible">
           <div className="gsap-parallax" data-speed="0.1">
             <div className="bg-unveilGreen w-[136px] h-[184px] sm:w-[410px] sm:h-[500px]">
               <Image
@@ -174,7 +176,7 @@ const FloatingArt = () => {
           </div>
         </div>
 
-        <div data-cursor="View Artist" data-cursor-color="#B6B0A4" className="gsap-thumbnail absolute block bottom-[0] sm:bottom-[30%] left-0 sm:left-auto sm:right-0 sm:translate-y-1/2 translate-y-0 z-10">
+        <div data-cursor="View Artist" data-cursor-color="#B6B0A4" className="gsap-thumbnail absolute block bottom-[0] sm:bottom-[30%] left-0 sm:left-auto sm:right-0 sm:translate-y-1/2 translate-y-0 z-10 invisible">
           <div className="gsap-parallax" data-speed="0.1">
             <div className="bg-unveilGreen w-[164px] h-[229px] sm:w-[320px] sm:h-[422px]">
               <Image
