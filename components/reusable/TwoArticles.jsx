60ac0362e12f57c5d64c8da868bd15fdb4041239
Baptiste Briel <baptiste@antinomy.studio>
2023-04-30 14:45:06 +0200

    added Animate component to Editiorial page

--- components/reusable/TwoArticles.jsx
+++ components/reusable/TwoArticles.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import { useRef } from "react";
+import Animate from "@/components/reusable/Animate";
+import { useLineMaskingAnimation } from "@/hooks/animations/useLineMaskingAnimation";
 
 const TwoArticles = () => {
+  const el = useRef();
+
+  useLineMaskingAnimation(el);
+
   return (
-    <section className="py-[120px] border-b border-b-unveilDrakGray mb-10 px-[15px] md:px-10">
+    <section ref={el} className="py-[120px] border-b border-b-unveilDrakGray mb-10 px-[15px] md:px-10">
       <h2 className="s2 mb-[15px]">Continue reading</h2>
-      <div className="grid grid-cols-2 gap-[15px] md:gap-10 border-t-[3px] border-t-unveilBlack pt-5">
-        <div className="grid-cols-2 gap-5 md:grid">
+      <div className="gsap-line border-t-[3px] border-t-unveilBlack"></div>
+      <div className="grid grid-cols-2 gap-[15px] md:gap-10 pt-5">
+        <Animate options={{ image: true, alpha: true }} className="grid-cols-2 gap-5 md:grid">
           <div className="bg-bgColor aspect-[3/4] w-full"></div>
           <div className="flex flex-col justify-end ">
             <p className="mt-2 l2">Educate</p>
@@ -13,8 +20,8 @@ const TwoArticles = () => {
               Trying to define yourself is like trying to bite your own teeth
             </p>
           </div>
-        </div>
-        <div className="grid-cols-2 gap-5 md:grid">
+        </Animate>
+        <Animate options={{ image: true, alpha: true }} className="grid-cols-2 gap-5 md:grid">
           <div className="bg-bgColor rounded-t-full aspect-[3/4] w-full"></div>
           <div className="flex flex-col justify-end ">
             <p className="mt-2 l2">Educate</p>
@@ -22,7 +29,7 @@ const TwoArticles = () => {
               Trying to define yourself is like trying to bite your own teeth
             </p>
           </div>
-        </div>
+        </Animate>
       </div>
     </section>
   );
--- components/section/editorial-page/TextImageTwo.jsx
+++ components/section/editorial-page/TextImageTwo.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import Animate from "@/components/reusable/Animate";
 
 const TextImageTwo = ({ data }) => {
   return (
     <section className="grid grid-cols-1 mb-10 md:my-20 md:grid-cols-5">
-      <div className="order-2 md:col-span-2 md:order-1">
+      <Animate options={{ alpha: true }} className="order-2 md:col-span-2 md:order-1">
         <p className="md:max-w-[350px] mt-10 leading-[110%] px-[15px] ml-[100px] md:ml-[40px] s2 drop-cap">
           {data.Text}
         </p>
-      </div>
+      </Animate>
       <div className="order-1 w-full md:col-span-3 md:order-2">
-        <div className="w-full bg-bgColor aspect-[10/11]">
-          <img
+        <Animate options={{ y: 0, image: true }} className="relative w-full bg-bgColor aspect-[10/11] overflow-hidden">
+          <Image
             src={data.Image.data.attributes.url}
             alt={data.Image.data.attributes.alt}
+            layout="fill"
+            objectFit="cover"
+            className="gsap-image"
           />
-        </div>
+        </Animate>
         <div className="md:bg-[#D9D9D9] md:py-20 md:text-center text-left px-[15px] py-2 b3 md:l2"></div>
         {data.Caption && (
-          <p className="mt-[15px] b4 hidden md:block">{data.Caption}</p>
+          <Animate options={{ alpha: true }} className="mt-[15px] hidden md:block">
+            <p className="b4">{data.Caption}</p>
+          </Animate>
         )}
       </div>
     </section>
