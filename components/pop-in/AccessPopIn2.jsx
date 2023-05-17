df9430f5b863c88f72fe646cbff64ef5bdcca3c7
oldevelopment <robin@orangeandlemons.nl>
2023-05-17 10:56:34 +0200

    popup form functionality

--- components/pop-in/AccessPopIn2.jsx
+++ components/pop-in/AccessPopIn2.jsx
@@ -6,52 +6,35 @@ import axios from "axios";
 const AccessPopIn2 = ({ accessOpen, setAccessOpen, data, i }) => {
   const el = useRef();
 
-  async function sendCrmRequest(){
-    const body = {
-      "contact": {
-        "id": 1,
-        "first_name": "Ammar",
-        "last_name": "Sampleton (sample)",
-        "display_name": "James Sampleton (sample)",
-        "avatar": null,
-        "job_title": null,
-        "city": null,
-        "state": null,
-        "zipcode": null,
-        "country": null,
-        "email": "ammaryousaf6@gmail.com",
-        "time_zone": null,
-        "work_number": null,
-        "mobile_number": "1-926-555-9503",
-        "address": null,
-        "last_seen": null,
-        "lead_score": 0,
-        "last_contacted": null,
-        "open_deals_amount": "0.0",
-        "links": {
-        },
-        "custom_field": {
-        },
-        "updated_at": "2016-06-01T02:54:41-07:00",
-        "keyword": null,
-        "medium": null,
-        "facebook": null,
-        "twitter": null,
-        "linkedin": null
-      }
-    };
-    const { data } = await axios({
+  const sendCrmRequest = async (e) =>{
+    e.preventDefault();
+    const name = document.querySelector('input[name="name"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const website = document.querySelector('input[name="website"]').value;
+    const type = document.querySelector('input[name="type-selected"]').value;
+    const {data}= await axios({
       method: "POST",
-      url: `https://unveil-org.myfreshworks.com/crm/sales/api/contacts`,
-      headers: {
-        "content-type": "application/json",
-        authorization: `Token token=x8o6DaVRqjb0YWPiPEdufw`,
-      },
-      data: body
+      url: `https://cumbersome-furtive-fowl.strapiapp.com/api/crm`,
+      data: {
+        "name" : name,
+        "email" : email,
+        "website" : website,
+        "type" : type
+    },
+      withCredentials: true,
     });
+    if(data.status == 200){
+      alert("Your request has been sent successfully");
+    } else {
+      console.log(data.message)
+      alert("You will be notified soon");
+    }
+    setAccessOpen(false);
     
-    console.log(data);
-    return data;
+  }
+
+  function changeType(params) {
+    document.querySelector('input[name="type-selected"]').value = params;
   }
 
   useAsideAnimation(el, accessOpen);
@@ -61,6 +44,7 @@ const AccessPopIn2 = ({ accessOpen, setAccessOpen, data, i }) => {
       ref={el}
       className="fixed z-50 invisible w-full h-screen overflow-hidden"
     >
+      
       <div className="gsap-el fixed overflow-y-scroll top-[15px] right-[15px] sm:top-5 sm:right-5 w-[280px] sm:w-[380px]  bg-[#ECE8DE] px-5 py-10 z-50 rounded-[20px] h-screen sm:h-fit">
         <div
           onClick={() => setAccessOpen(false)}
@@ -76,20 +60,25 @@ const AccessPopIn2 = ({ accessOpen, setAccessOpen, data, i }) => {
         <h3 className="mb-[60px] text-center s2 max-w-[270px] mx-auto">
           {data ? data.heading : "Show us your work and get access to Unveil"}
         </h3>
+        {/* <h4 className="mb-[20px] text-center s2 max-w-[270px] mx-auto">Your request has been sent successfully</h4> */}
         <p className="b3 leading-[16px] text-[11px] max-w-[250px]">
           {data
             ? data.description
             : "Provide a link to your work and submitting a series of works upon selection by our curator."}
         </p>
-        <form className="mt-5 ">
-          <input type="text" placeholder="Name" className="input" />
-          <input type="email" placeholder="Email" className="my-1 input" />
+        <form className="mt-5 " onSubmit={sendCrmRequest}>
+          <input type="text" required name="name" id="name" placeholder="Name" className="input" />
+          <input type="email" required name="email" id="email"  placeholder="Email" className="my-1 input" />
           <input
-            type="text"
+            type="url"
             placeholder="Website (or social)"
             className="input"
+            required
+            id="website"
+            name="website"
           />
           <p className="py-5">i&apos;m a:</p>
+          <input type="hidden" id="type-selected" name="type-selected" value="artist" />
           <div className="grid grid-cols-2 pb-[15px]">
             <div>
               <input
@@ -98,7 +87,7 @@ const AccessPopIn2 = ({ accessOpen, setAccessOpen, data, i }) => {
                 name="type"
                 id={`artist${i}`}
               />
-              <label htmlFor={`artist${i}`}>Artist</label>
+              <label htmlFor={`artist${i}`} onClick={() => changeType('artist')}>Artist</label>
             </div>
             <div>
               <input
@@ -107,11 +96,11 @@ const AccessPopIn2 = ({ accessOpen, setAccessOpen, data, i }) => {
                 name="type"
                 id={`gallery${i}`}
               />
-              <label htmlFor={`gallery${i}`}>Gallery</label>
+              <label htmlFor={`gallery${i}`} onClick={() => changeType('gallery')}>Gallery</label>
             </div>
           </div>
 
-          <button className="btn btn-full btn-primary btn-big" onClick={() => sendCrmRequest()}>
+          <button className="btn btn-full btn-primary btn-big" type="submit">
             Apply now
           </button>
         </form>
