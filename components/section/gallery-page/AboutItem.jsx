eb4b64b3d99fd8be3a19db67d0f338ef31146c5e
Mike Schaper <mikeschaper12@gmail.com>
2023-06-09 14:53:26 +0200

    Added key to about item

--- components/section/gallery-page/AboutItem.jsx
+++ components/section/gallery-page/AboutItem.jsx
@@ -8,8 +8,11 @@ const AboutItem = ({ detail_shots, owner }) => {
   return (
     <div className="block w-full py-10 md:py-[90px] overflow-hidden">
       <div className="gsap-scroll flex flex-col md:flex-row flex-nowrap gap-10 px-[15px] md:px-10">
-        {detail_shots.map((item) => (
-          <div className="w-full md:w-auto md:min-w-[66%] md:max-w-[66%] flex-col md:flex-row flex gap-5 md:gap-10">
+        {detail_shots.map((item, i) => (
+          <div
+            key={i}
+            className="w-full md:w-auto md:min-w-[66%] md:max-w-[66%] flex-col md:flex-row flex gap-5 md:gap-10"
+          >
             <div className="ml-[80px] md:ml-0 w-[230px] md:w-[160px] b3 md:b4 order-2 md:order-1">
               <p className="leading-[18px] b4">{item.caption}</p>
               {item.audio_url && (
