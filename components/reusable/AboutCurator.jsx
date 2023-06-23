85a5b55db087925b6744b7dfb5028d74be777951
Mike Schaper <mikeschaper12@gmail.com>
2023-06-23 17:54:51 +0200

    line clmap

--- components/reusable/AboutCurator.jsx
+++ components/reusable/AboutCurator.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { getArtistRecognitions, unfollowArtist, isFollowed, postFollower, getUserMe } from "lib/backend";
+import {
+  getArtistRecognitions,
+  unfollowArtist,
+  isFollowed,
+  postFollower,
+  getUserMe,
+} from "lib/backend";
 import Loader from "../svg/Loader";
 
 const AboutCurator = ({ owner }) => {
-
   const [recognitions, setRecognitions] = useState([]);
-  const [following, setFollowing] = useState(false)
-  const [loading, setLoading] = useState(false)
-  const [authUser, setAuthUser] = useState({})
+  const [following, setFollowing] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [authUser, setAuthUser] = useState({});
 
   // Owner name to string
   let displayName;
@@ -24,47 +29,45 @@ const AboutCurator = ({ owner }) => {
   }
 
   const handleFollowUnfollow = (isFollow) => {
-    setLoading(true)
-    const token = localStorage.getItem('token')
-    if(isFollow) {
+    setLoading(true);
+    const token = localStorage.getItem("token");
+    if (isFollow) {
       //unfollow code
       unfollowArtist(token, { user_id: owner.id })
         .then((res) => setFollowing(false))
-        .catch(err => console.log(err))
-        .finally(() => setLoading(false))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     } else {
       // follow code
-      postFollower(token, {user_id: owner.id})
+      postFollower(token, { user_id: owner.id })
         .then((res) => setFollowing(true))
-        .catch(err => console.log(err))
-        .finally(() => setLoading(false))
+        .catch((err) => console.log(err))
+        .finally(() => setLoading(false));
     }
-  }
+  };
 
-  const getRecognitions = async(artist_id)  => {
+  const getRecognitions = async (artist_id) => {
     const data = await getArtistRecognitions(artist_id);
     setRecognitions(data);
-  }
+  };
   useEffect(() => {
-    if(owner.id){
-      getRecognitions(owner.id)
-      const token = localStorage.getItem('token')
-      setLoading(true)
+    if (owner.id) {
+      getRecognitions(owner.id);
+      const token = localStorage.getItem("token");
+      setLoading(true);
       isFollowed(token, owner.id)
         .then((res) => setFollowing(res.data))
         .catch((err) => {
-          console.log(err)
-          setFollowing(false)
+          console.log(err);
+          setFollowing(false);
         })
-        .finally(() => setLoading(false))
+        .finally(() => setLoading(false));
 
-      
       getUserMe(token)
-        .then(res => setAuthUser(res))
-        .catch(err => console.log(err))
-        
+        .then((res) => setAuthUser(res))
+        .catch((err) => console.log(err));
     }
-  },[owner]);
+  }, [owner]);
 
   return (
     <section className="grid grid-cols-1 md:grid-cols-2">
@@ -85,39 +88,45 @@ const AboutCurator = ({ owner }) => {
         <div className="w-[55%] pt-[100px] md:pt-0 pr-[15px] pb-10 ml-auto md:ml-0 md:absolute bottom-10 right-10  h-fit">
           <div className="flex items-end gap-2 flex-nowrap">
             <p className="b3">By</p>
-            <Link href={`/people/${owner.id}`}><p className="truncate l2">{displayName}</p></Link>
+            <Link href={`/people/${owner.id}`}>
+              <p className="truncate l2">{displayName}</p>
+            </Link>
           </div>
 
-          <h4 className="mt-5 mb-10 b2 md:h2">{owner.description}</h4>
+          <h4 className="mt-5 mb-10 overflow-hidden b2 md:h2 line-clamp-[10] md:line-clamp-6">
+            {owner.description}
+          </h4>
           <div className="mb-5">
-            {
-              recognitions.map(({id, recognition_type, description}) => (
-                <p key={id} className="py-1 border-b cursor-pointer b3 md:b4 border-unveilWhite">
-              x {description} ({recognition_type})
-            </p>
-              ))
-            }
-            
+            {recognitions.map(({ id, recognition_type, description }) => (
+              <p
+                key={id}
+                className="py-1 border-b cursor-pointer b3 md:b4 border-unveilWhite"
+              >
+                x {description} ({recognition_type})
+              </p>
+            ))}
+
             <p className="py-1 truncate w-[100px] cursor-pointer b3 md:b4 border-unveilWhite">
-              {owner?.walletAddress?.slice(0,4).toLowerCase()}...{owner?.walletAddress?.slice(-4).toLowerCase()}
+              {owner?.walletAddress?.slice(0, 4).toLowerCase()}...
+              {owner?.walletAddress?.slice(-4).toLowerCase()}
             </p>
           </div>
-          { 
-            owner.id !== authUser.id && (
-              <button 
-                className="btn btn-secondary hover:bg-[#292928] btn-full border-unveilWhite"
-                onClick={() => handleFollowUnfollow(following)}
-              >
-                {
-                  loading ? (
-                    <div className="h-[25px] animate-spin justify-center flex items-center">
-                      <Loader color="#F7F4ED" />
-                    </div>
-                  ) : following ? `Following` : `Follow artist`
-                }
-              </button>
-            )
-          }
+          {owner.id !== authUser.id && (
+            <button
+              className="btn btn-secondary hover:bg-[#292928] btn-full border-unveilWhite"
+              onClick={() => handleFollowUnfollow(following)}
+            >
+              {loading ? (
+                <div className="h-[25px] animate-spin justify-center flex items-center">
+                  <Loader color="#F7F4ED" />
+                </div>
+              ) : following ? (
+                `Following`
+              ) : (
+                `Follow artist`
+              )}
+            </button>
+          )}
         </div>
       </div>
     </section>
