6904fe48fba13a01f03217770e73b1f5f8b77c85
Shekhar Kumar <shekharkumar235838@gmail.com>
2023-06-26 10:57:06 +0530

    hotjar analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import Layout from "../components/general/Layout";
 import Web3AuthProvider from "@/contexts/Web3AuthContext";
 import { StepProvider } from "@/contexts/StepContext";
 import "react-toastify/dist/ReactToastify.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   const lenis = useLenis();
@@ -39,6 +40,20 @@ function MyApp({ Component, pageProps }) {
               }}
             >
               <Component {...pageProps} />
+              <Script
+                id="HotJarAnalytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                  __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3548666,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+                }}
+              />
             </Lenis>
           </HistoryProvider>
         </Layout>
