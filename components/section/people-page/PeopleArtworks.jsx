69083a754977ef6d56d3fd28d10c944fc310e449
fullStack-pureBillion <132538073+fullStack-pureBillion@users.noreply.github.com>
2023-06-26 16:01:21 +0530

    making the splitarray as global function

--- components/section/people-page/PeopleArtworks.jsx
+++ components/section/people-page/PeopleArtworks.jsx
@@ -3,60 +3,9 @@ import AboutStats from "../../reusable/AboutStats";
 import { getArtworkByArtistId } from "lib/backend"
 import GalleryBlockItems from "../GalleryBlockItems";
 import Loader from "@/components/svg/Loader";
+import { splitArrayByPattern } from 'lib/utils'
 
-const getPattern = (length) => {
-  switch (length) {
-    case 9:
-      return [3, 2, 3, 1];
-    case 8:
-      return [3, 2, 3];
-    case 7:
-      return [3, 1, 2, 1];
-    case 6:
-      return [3, 1, 2];
-    case 5:
-      return [3, 2];
-    case 4:
-      return [3, 1];
-    case 3:
-      return [3];
-    case 2:
-      return [2];
-    case 1:
-      return [1];
-    default:
-      return [length];
-  }
-};
 
-const splitArrayByPattern = (arr, variant) => {
-  let pattern;
-
-  if (arr.length < 9) {
-    // If the array length is less than 9, always use pattern 1
-    pattern = getPattern(arr.length);
-  } else {
-    // Otherwise, select the pattern based on the variant
-    const patterns = [
-      getPattern(arr.length),
-      [3, 1, 3, 2],
-      [1, 3, 2, 3],
-      [2, 3, 1, 3],
-    ];
-    pattern = patterns[variant - 1];
-  }
-
-  let subArrays = [];
-  let currentIndex = 0;
-
-  for (const count of pattern) {
-    const slice = arr.slice(currentIndex, currentIndex + count);
-    subArrays.push(slice);
-    currentIndex += count;
-  }
-
-  return subArrays;
-};
 
 const PeopleArtworks = ({ userId }) => {
 
