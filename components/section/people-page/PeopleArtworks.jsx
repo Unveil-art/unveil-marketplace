63db495d712b77f28685065adf5499821ca6a5c5
Shekhar Kumar <shekharkumar235838@gmail.com>
2023-06-27 22:14:31 +0530

    account page changes

--- components/section/people-page/PeopleArtworks.jsx
+++ components/section/people-page/PeopleArtworks.jsx
@@ -10,6 +10,7 @@ import { splitArrayByPattern } from 'lib/utils'
 const PeopleArtworks = ({ userId }) => {
 
   const [artwork, setArtwork] = useState([])
+  const [totalArts, setTotalArts] = useState(0);
   const [variant, setVariant] = useState(1)
   const [loading, setLoading] = useState(false)
   // state for pagination in future
@@ -23,6 +24,7 @@ const PeopleArtworks = ({ userId }) => {
     setLoading(true)
     try {
       const arts = await getArtworkByArtistId(userId)
+      setTotalArts(arts.length ?? 0)
       const _variant = Math.floor(Math.random() * 2) + 1;
       setVariant(_variant)
       const artResult = splitArrayByPattern(arts, _variant)
@@ -39,13 +41,13 @@ const PeopleArtworks = ({ userId }) => {
       <AboutStats
         bigLetters
         h1="artworks"
-        b1="21"
+        b1={totalArts}
         h2="New works this year"
-        b2="18"
+        b2={totalArts}
         h3="Highest sale"
-        b3="€2093"
+        b3="$0"
         h4="Average increase"
-        b4="53%"
+        b4="0%"
       />
       {/* <ThreeBlockItems /> */}
       {
