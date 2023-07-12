b7d6b5c37f1f1e2ada332a7c554ee46838aac89d
Shekhar Kumar <shekharkumar235838@gmail.com>
2023-07-12 21:49:38 +0530

    set correct id

--- pages/_app.js
+++ pages/_app.js
@@ -67,7 +67,7 @@ function MyApp({ Component, pageProps }) {
                 dangerouslySetInnerHTML={{
                   __html: `(function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3548666,hjsv:6};
+        h._hjSettings={hjid:3569036,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
